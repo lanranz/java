aebaa3c68971ffb7277fb4c8993d05f14cb66fb4
lanranz <2377259017@qq.com>
2020-08-11 18:59:42 +0800

    更新作业

---
+++ 8.11/04.PhoneSimulator/Que/src/PhoneCallGenerator.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+/**
+ * 生成对特定电话的呼叫
+ * 用于向电话呼叫
+ */
+public class PhoneCallGenerator extends Thread {
+
+    private CellularPhone phone;
+
+    public PhoneCallGenerator(String name, CellularPhone thePhone) {
+        super(name);
+        phone = thePhone;
+    }
+
+    public void run() {
+        Random random = new Random();
+        // 生成20个电话
+        synchronized (phone){
+            for (int i = 0; i < 20; i++) {
+                phone.startCall(getName(), String.valueOf(i));
+                int time = random.nextInt(3);
+                try {
+                    phone.join(time * 100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                phone.endCall(getName(), String.valueOf(i));
+            }
+        }
+
+    }
+
+}
